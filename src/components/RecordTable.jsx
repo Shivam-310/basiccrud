2e680a55ba3083fad7ecf29b6747fca5f43c7f5a
Shivam <shivamchaudhary310899@.com>
2025-12-22 20:05:33 +0530

    basic setup

---
+++ src/components/RecordTable.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function RecordTable() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-6">
+      <div className="max-w-7xl mx-auto">
+        <div className="bg-white rounded-lg shadow-md p-6 mb-6">
+            <h1 className="font-3xl font-bold text-gray-800 mb-2">Employee Management</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RecordTable
