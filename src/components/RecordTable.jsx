414dc98ac10e2cc318044001a4642499c644d96f
Shivam <shivamchaudhary310899@.com>
2025-12-23 16:58:36 +0530

    button

--- src/components/RecordTable.jsx
+++ src/components/RecordTable.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Plus } from 'lucide-react'
 
 function RecordTable() {
   return (
@@ -13,12 +13,22 @@ function RecordTable() {
         <div className='bg-white rounded-lg shadow-md p-6 mb-6'>
             <div className='flex flex-col md:flex-row gap-4'>
                 <div>
-                    <search className='absolute left-3 top-1/2 transform-translate-y-1/2 text-gray-400 size={20}'/>
-                    <input type='text' placeholder='search by name, email or position' className='w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus-ring-blue-500'/>
+                    <search className='absolute left-3 top-1/2 transform-translate-y-1/2 text-gray-400 size={20}' />
+                    <input type='text' placeholder='search by name, email or position' className='w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus-ring-blue-500' />
                 </div>
+                <button className='flex items-center justify-center gap-2 bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-all'>
+                    <Plus size={20} />
+                Add New Record
+                </button>
             </div>
-
         </div>
+
+        <div className='bg-white rounded-lg shadow-md overflow-hidden'>
+            <div className='overflow-x-auto'>
+
+            </div>
+        </div>
+
       </div>
     </div>
   )
