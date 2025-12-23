9cac6cbbcaafb75f84b1edede44dfa54c95ffa1f
Shivam <shivamchaudhary310899@.com>
2025-12-23 16:23:25 +0530

    setting up the search bar

--- src/components/RecordTable.jsx
+++ src/components/RecordTable.jsx
@@ -6,6 +6,18 @@ function RecordTable() {
       <div className="max-w-7xl mx-auto">
         <div className="bg-white rounded-lg shadow-md p-6 mb-6">
             <h1 className="font-3xl font-bold text-gray-800 mb-2">Employee Management</h1>
+            <p className='text-gray-600'>
+                Manage Employee Record with Redux Toolkit
+            </p>
+        </div>
+        <div className='bg-white rounded-lg shadow-md p-6 mb-6'>
+            <div className='flex flex-col md:flex-row gap-4'>
+                <div>
+                    <search className='absolute left-3 top-1/2 transform-translate-y-1/2 text-gray-400 size={20}'/>
+                    <input type='text' placeholder='search by name, email or position' className='w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus-ring-blue-500'/>
+                </div>
+            </div>
+
         </div>
       </div>
     </div>
