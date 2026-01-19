17e4a1d61ddd8a7f094b5840021258e94a1ee74a
Shivam <shivamchaudhary310899@gmail.com>
2026-01-19 14:05:42 +0530

    redux

--- src/store/recordsSlide.js
+++ src/store/recordsSlide.js
@@ -2,5 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const recordsSlide = createSlice ({
     name: "records",
-    
-})
+    initialState: {
+        items: "",
+        seachTerm: "",
+        nextId: "", 
+    },
+
+    reducers: {},
+});
+
+export default recordsSlide.reducer;
