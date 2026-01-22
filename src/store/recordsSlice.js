2e3b0cc7c25a00317becb49c2096666e1568c54c
Shivam <shivamchaudhary310899@gmail.com>
2026-01-22 11:14:09 +0530

    managing state

--- src/store/recordsSlice.js
+++ src/store/recordsSlice.js
@@ -32,6 +32,15 @@ const demoRecords = [
 ];
 
 
+// load records from local storage or demo data
+const LoadRecordsFromStorage = () => {
+    try {
+        const saveRecords = localStorage.getItem("employeeRecords");
+        return saveRecords ? JSON.parse(saveRecords) : demoRecords;
+    }catch(error){console.error("error loading", error);
+    }
+};
+
 // Calculate nextId based on demoRecords
 const calculateNextId = (records) => {
     if (!records || records.length === 0) return 1;
@@ -44,9 +53,10 @@ const recordsSlice = createSlice ({
     initialState: {
         items: demoRecords,
         seachTerm: "",
-        nextId: calculateNextId(demoRecords), 
+        nextId: calculateNextId(LoadRecordsFromStorage()), 
     },
 
+    // add new record
     reducers: {
         addRecord: (state, action) => {
             const newRecord = {id: state.nextId, ...action.payload};
@@ -55,7 +65,55 @@ const recordsSlice = createSlice ({
             localStorage.setItem("records", JSON.stringify(state.items));
             state.nextId = calculateNextId(state.items);
     },
+    
+    // upadate record
+    updatedRecord: (state, action) => {
+        const {id, data} = action.payload;
+        const index = state.items.findIndex((r) => r.id === id);
+
+        if (index !== -1){
+            state.items[index] = {...state.items[index], ...data};
+            localStorage.setItem("records", JSON.stringify(state.items));
+        }
+    },
+
+    // delete record
+    deleteRecord: (state, action) => {
+        state.items = state.items.filter((r) => r.id !== action.payload);
+        localStorage.setItem("employeeRecords", JSON.stringify(state.items));
+        state.nextId = calculateNextId(state.items);
+    },
+
+    // search record
+    setSearchTerm: (state, action) => {
+        state.seachTerm = action.payload;
+    },
+    resetAllRecords: (state) => {
+        state.items = demoRecords;
+        state.nextId = calculateNextId(demoRecords);
+        localStorage.setItem("employeeRecords", JSON.stringify(state.items));
+    }
     },
 });
+
+export const { addRecord, 
+    updatedRecord, 
+    deleteRecord, 
+    setSearchTerm, 
+    resetAllRecords,
+ } = recordsSlice.actions;
+
+//  selectors
+export const selectAllRecords = (state) => state.records.items;
+export const selectSearchTerm = (state) => state.records.seachTerm;
+
+export const selectFilteredRecords = (state) => {
+    const term = state.records.seachTerm.toLowerCase();
+    return state.records.items.filter((r) =>
+        r.name.toLowerCase().includes(term) ||
+        r.email.toLowerCase().includes(term) || 
+        r.position.toLowerCase().includes(term)
+);
+};
 
 export default recordsSlice.reducer;
